ef5457c8ee7d590d84ac7aaf8e9b4134f36641d2
Miguel Angel Salas Flores <miguelangel.sf@outlook.com>
2023-06-24 00:45:34 -0600

    Feat: Principal structure -Server and database connection ready

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+export const dataBase = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost/todoapp')
+        console.log("//// Base de datos Conectada ////")
+    } catch (error) {
+        console.log(error)
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import app from './app.js'
+import {dataBase} from './db.js';
+
+dataBase();
+app.listen(3000)
+console.log('Servidor corriendo sobre el puerto', 3000);
