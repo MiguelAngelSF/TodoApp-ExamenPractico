acc0ee66f30447d5dbb7fe7cac08b6556782dd23
Miguel Angel Salas Flores <miguelangel.sf@outlook.com>
2023-06-25 11:14:13 -0600

    Fix: Module config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const TOKEN_SECRET = 'secret';
 export const PORT = process.env.PORT || 3000;
-export const DB = process.env.DB || 'mongodb+srv://miguelsf:abc123def456@cluster0.upmpcpb.mongodb.net/?retryWrites=true&w=majority'
+export const DB = process.env.DB || 'mongodb+srv://miguelsf:abc123def456@cluster0.upmpcpb.mongodb.net/test';
